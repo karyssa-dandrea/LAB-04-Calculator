1338dc34beeba23ac5968dee9e69b11236e92a7d
karyssa-dandrea <karyssadandrea@gmail.com>
2021-08-26 11:31:16 -0700

    created ui for add

--- app.js
+++ app.js
@@ -2,8 +2,21 @@
 
 // reference needed DOM elements
 
+const addNumber1 = document.getElementById('add-number-1');
+const addNumber2 = document.getElementById('add-number-2');
+const addButton = document.getElementById('add-button');
+const addAnswer = document.getElementById('add-answer');
+
 
 // set event listeners 
+
+addButton.addEventListener('click', () => {
+
+    
+})
+
+
+
     // get user input(s)
     // do any needed work with the value(s)
     // update DOM to reflect new value(s)
