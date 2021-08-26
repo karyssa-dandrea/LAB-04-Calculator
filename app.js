2dfc57ff9b49e799615c3d03ed6124c24b6d198e
karyssa-dandrea <karyssadandrea@gmail.com>
2021-08-26 14:30:30 -0700

    subtraction added

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // import functions
 import { add } from './calculator.js';
-
+import { sub } from './calculator.js';
 
 // reference needed DOM elements
 
@@ -9,6 +9,12 @@ const addNumber2 = document.getElementById('add-number-2');
 const addButton = document.getElementById('add-button');
 const addAnswer = document.getElementById('add-answer');
 
+const subNumber1 = document.getElementById('sub-number-1');
+const subNumber2 = document.getElementById('sub-number-2');
+const subButton = document.getElementById('sub-button');
+const subAnswer = document.getElementById('sub-answer');
+
+
 // set event listeners 
 
 addButton.addEventListener('click', () => {
@@ -16,6 +22,15 @@ addButton.addEventListener('click', () => {
     const num2 = Number(addNumber2.value);
     const result = add(num1, num2);
     addAnswer.textContent = result;
+    
+
+});
+
+subButton.addEventListener('click', () => {
+    const num1 = Number(subNumber1.value);
+    const num2 = Number(subNumber2.value);
+    const result = sub(num1, num2);
+    subAnswer.textContent = result;
     
 
 });
--- test/tests.js
+++ test/tests.js
@@ -1,6 +1,8 @@
 // IMPORT MODULES under test here:
 import { add } from '../calculator.js';
 
+import { sub } from '../calculator.js';
+
 const test = QUnit.test;
 
 // name your test by what it is testing
@@ -10,11 +12,27 @@ test('add two numbers', (expect) => {
     const number1 = 7;
     const number2 = 3;
     const expected = 10;
+
+    test('sub two numbers', (expect) => {
+        //Arrange
+        // Set up your arguments and expectations
+        const number1 = 20;
+        const number2 = 5;
+        const expected = 15;
     
     //Act 
     const result = add(number1, number2);
     // Call the function you're testing and set the result to a const
     const actual = result; // use your function here
+
+    //Act 
+    const result = sub(number1, number2);
+    // Call the function you're testing and set the result to a const
+    const actual = result; // use your function here
+
+    //Expect
+    // Make assertions about what is expected versus the actual result
+    expect.equal(actual, expected);
 
     //Expect
     // Make assertions about what is expected versus the actual result
